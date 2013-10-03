7010a1da9ed18ef44b4c08d17eed2ff9f1ad90ce
소영섭 <soMac@soyeongseob-ui-MacBook-Pro.local>
2013-10-04 05:15:05 +0900

    Initial Commit

---
+++ CalulatorApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalulatorApp
+//
+//  Created by 소영섭 on 13. 10. 4..
+//  Copyright (c) 2013년 소영섭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalulatorApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CalulatorApp
+//
+//  Created by 소영섭 on 13. 10. 4..
+//  Copyright (c) 2013년 소영섭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
